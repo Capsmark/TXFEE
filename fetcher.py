1a494214e67e1a8d74b0a10d3a360b3e6c97a2ea
Amir <farhaadi.amir@gmail.com>
2023-10-30 15:21:10 +0330

    ✨ Feat: added merge and all functions calls to main

--- fetcher.py
+++ fetcher.py
@@ -1,5 +1,6 @@
 import csv
 import requests
+import pandas as pd
 from datetime import datetime, timedelta
 
 old_format = '%Y-%m-%d %H:%M:%S'
@@ -20,8 +21,10 @@ def write_to_file(data, csv_file):
             writer.writerow(row)
 
 
-def fetch_api(from_time, to_time, limit, url):
+def fetch_api(from_time, to_time, limit, base_url):
     all_data = []
+    url = (f"{base_url}?window=day&limit=100000&"
+           f"from={from_time}&to={to_time}")
 
     while from_time <= limit:
         response = requests.get(url, headers=headers).json()
@@ -34,9 +37,32 @@ def fetch_api(from_time, to_time, limit, url):
         from_time = to_time
         to_time = (datetime.strptime(to_time, api_format) +
                    timedelta(days=1)).strftime(api_format)
-        url = f"https://api.cryptoquant.com/v1/eth/market-data/price-ohlcv?window=day&&limit=100000&from={from_time}&to={to_time}"
+        url = (f"{base_url}?window=day&limit=100000&"
+               f"from={from_time}&to={to_time}")
 
     return all_data
+
+
+def merge_csv_files():
+    # List of file paths for the 4 CSV files
+    file_paths = ["./new/fees.csv", "./new/addresses-count.csv",
+                  "./new/price-ohlcv.csv", "./new/transactions-count.csv"]
+
+    # List to store the dataframes of each CSV file
+    dataframes = []
+
+    # Read each CSV file and store its dataframe in the list
+    for file_path in file_paths:
+        df = pd.read_csv(file_path)
+        dataframes.append(df)
+
+    # Merge dataframes based on the "date" column
+    merged_df = pd.merge(dataframes[0], dataframes[1], on="date", how="inner")
+    merged_df = pd.merge(merged_df, dataframes[2], on="date", how="inner")
+    merged_df = pd.merge(merged_df, dataframes[3], on="date", how="inner")
+
+    # Save the merged dataframe to a new CSV file
+    merged_df.to_csv("./merged.csv", index=False)
 
 
 def main():
@@ -44,23 +70,29 @@ def main():
     from_time = '20200101T000000'
     to_time = (datetime.strptime(from_time, api_format) +
                timedelta(days=100000)).strftime(api_format)
-    limit = '20231023T000000'
+    limit = '20231101T000000'
 
-    # url = f"https://api.cryptoquant.com/v1/eth/network-data/transactions-count?window=day&limit=100000&from={from_time}&to={to_time}"
-    # data = fetch_api(from_time, to_time, limit, url)
-    # write_to_file(data, 'new/transactions-count.csv')
+    name = "transactions-count"
+    base_url = f"https://api.cryptoquant.com/v1/eth/network-data/{name}"
+    data = fetch_api(from_time, to_time, limit, base_url)
+    write_to_file(data, f"./new/{name}.csv")
 
-    # url = f"https://api.cryptoquant.com/v1/eth/network-data/fees?window=day&limit=100000&from={from_time}&to={to_time}"
-    # data = fetch_api(from_time, to_time, limit, url)
-    # write_to_file(data, 'new/fees.csv')
+    name = "fees"
+    base_url = f"https://api.cryptoquant.com/v1/eth/network-data/{name}"
+    data = fetch_api(from_time, to_time, limit, base_url)
+    write_to_file(data, f"./new/{name}.csv")
 
-    # url = f"https://api.cryptoquant.com/v1/eth/network-data/addresses-count?window=day&limit=100000&from={from_time}&to={to_time}"
-    # data = fetch_api(from_time, to_time, limit, url)
-    # write_to_file(data, 'new/addresses-count.csv')
+    name = "addresses-count"
+    base_url = f"https://api.cryptoquant.com/v1/eth/network-data/{name}"
+    data = fetch_api(from_time, to_time, limit, base_url)
+    write_to_file(data, f"./new/{name}.csv")
 
-    url = f"https://api.cryptoquant.com/v1/eth/market-data/price-ohlcv?window=day&&limit=100000&from={from_time}&to={to_time}"
-    data = fetch_api(from_time, to_time, limit, url)
-    write_to_file(data, 'new/price-ohlcv.csv')
+    name = "price-ohlcv"
+    base_url = f"https://api.cryptoquant.com/v1/eth/market-data/{name}"
+    data = fetch_api(from_time, to_time, limit, base_url)
+    write_to_file(data, f"./new/{name}.csv")
+
+    merge_csv_files()
 
 
 if __name__ == "__main__":
